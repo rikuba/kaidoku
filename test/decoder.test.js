88c9340fc2055902a1f89256ebe9fee3c16b459f
Rikuba <rikuba@live.jp>
2021-06-13 13:04:02 +0900

    テスト用の文章を英数字を含む文章に変更

--- test/decoder.test.js
+++ test/decoder.test.js
@@ -3,21 +3,23 @@
 const assert = require('assert')
 const { decode } = require('../lib/decoder')
 
-const sampleText = `吾輩は猫である。名前はまだ無い。
+const sampleText = `これはテスト文書です。
 
-どこで生れたかとんと見当がつかぬ。
-何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
-吾輩はここで始めて人間というものを見た。
+・Base64
+・Quoted Printable
+・パーセントエンコーディング
+
+いずれかの形式により符号化されています。
 `
 
 const encodings = ['euc-jp', 'iso-2022-jp', 'shift_jis', 'utf-8']
 
 describe('Base64', () => {
   const encodedTexts = {
-    'euc-jp': 'uOPH2qTPx62kx6SipOuho8y+wbCkz6TepMDMtaSkoaMKCqTJpLOkx8C4pOykv6SrpMik86TIuKvF9qSspMSkq6TMoaMKsr+kx6Tix/awxaSkpLik4aS4pOGkt6S/veqkx6XLpeOhvKXLpeOhvLXjpKSkxqSkpL+79qTApLGkz7WtsrGkt6TGpKSk66GjCrjjx9qkz6SzpLOkx7vPpOGkxr/NtNakyKSkpKak4qTOpPK4q6S/oaMK',
-    'iso-2022-jp': 'GyRCOGNHWiRPRy0kRyQiJGshI0w+QTAkTyReJEBMNSQkISMbKEIKChskQiRJJDMkR0A4JGwkPyQrJEgkcyRIOCtFdiQsJEQkKyRMISMbKEIKGyRCMj8kRyRiR3YwRSQkJDgkYSQ4JGEkNyQ/PWokRyVLJWMhPCVLJWMhPDVjJCQkRiQkJD87diRAJDEkTzUtMjEkNyRGJCQkayEjGyhCChskQjhjR1okTyQzJDMkRztPJGEkRj9NNFYkSCQkJCYkYiROJHI4KyQ/ISMbKEIK',
-    shift_jis: 'jOGUeYLNlEyCxYKggumBQpa8kU+CzYLcgr6Ws4KigUIKCoLHgrGCxZC2guqCvYKpgsaC8YLGjKmTloKqgsKCqYLKgUIKib2CxYLglJaIw4KigraC34K2gt+CtYK9j4qCxYNqg4OBW4Nqg4OBW4uDgqKCxIKigr2OloK+gq+CzYtMia+CtYLEgqKC6YFCCozhlHmCzYKxgrGCxY5ugt+CxJBsitSCxoKigqSC4ILMgvCMqYK9gUIK',
-    'utf-8': '5ZC+6Lyp44Gv54yr44Gn44GC44KL44CC5ZCN5YmN44Gv44G+44Gg54Sh44GE44CCCgrjganjgZPjgafnlJ/jgozjgZ/jgYvjgajjgpPjgajopovlvZPjgYzjgaTjgYvjgazjgIIK5L2V44Gn44KC6JaE5pqX44GE44GY44KB44GY44KB44GX44Gf5omA44Gn44OL44Oj44O844OL44Oj44O85rOj44GE44Gm44GE44Gf5LqL44Gg44GR44Gv6KiY5oa244GX44Gm44GE44KL44CCCuWQvui8qeOBr+OBk+OBk+OBp+Wni+OCgeOBpuS6uumWk+OBqOOBhOOBhuOCguOBruOCkuimi+OBn+OAggo='
+    'euc-jp': 'pLOk7KTPpcaluaXIyri98aTHpLmhowoKoaZCYXNlNjQKoaZRdW90ZWQgUHJpbnRhYmxlCqGmpdGhvKW7pfOlyKWopfOls6G8pcelo6XzpbAKCqSkpLqk7KSrpM63wbywpMuk6KTqyeS55rK9pLWk7KTGpKSk3qS5oaMK',
+    'iso-2022-jp': 'GyRCJDMkbCRPJUYlOSVISjg9cSRHJDkhIxsoQgoKGyRCISYbKEJCYXNlNjQKGyRCISYbKEJRdW90ZWQgUHJpbnRhYmxlChskQiEmJVEhPCU7JXMlSCUoJXMlMyE8JUclIyVzJTAbKEIKChskQiQkJDokbCQrJE43QTwwJEskaCRqSWQ5ZjI9JDUkbCRGJCQkXiQ5ISMbKEIK',
+    shift_jis: 'grGC6oLNg2WDWINnlbaPkYLFgreBQgoKgUVCYXNlNjQKgUVRdW90ZWQgUHJpbnRhYmxlCoFFg3CBW4Nag5ODZ4NHg5ODUoFbg2aDQoOTg08KCoKigriC6oKpgsyMYI6ugsmC5oLolYSNhom7grOC6oLEgqKC3IK3gUIK',
+    'utf-8': '44GT44KM44Gv44OG44K544OI5paH5pu444Gn44GZ44CCCgrjg7tCYXNlNjQK44O7UXVvdGVkIFByaW50YWJsZQrjg7vjg5Hjg7zjgrvjg7Pjg4jjgqjjg7PjgrPjg7zjg4fjgqPjg7PjgrAKCuOBhOOBmuOCjOOBi+OBruW9ouW8j+OBq+OCiOOCiuespuWPt+WMluOBleOCjOOBpuOBhOOBvuOBmeOAggo='
   }
 
   for (const encoding of encodings) {
